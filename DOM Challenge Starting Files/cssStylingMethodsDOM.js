ba22bd4ba27024c2dae4f23bd1d3c1a68ca4e904
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-07 22:45:49 +0530

    DOM

---
+++ DOM Challenge Starting Files/cssStylingMethodsDOM.js
@@ -0,0 +1,27 @@
+var element;
+
+//element = document.querySelector("#header").style.border;
+
+//element = document.querySelector("#header").style.color;
+
+//document.querySelector("#header").style.backgroundColor = "aqua";
+
+//document.querySelector("#header").style.color = "blue";
+
+//document.querySelector("#header").className = "abc xyz";
+
+//element = document.querySelector("#header").className;
+
+//document.querySelector("#header").classList = "abc xyz";
+
+//element = document.querySelector("#header").classList;
+
+//document.querySelector("#header").classList.add("xyz","efg");
+
+//element = document.querySelector("#header").classList;
+
+//document.querySelector("#header").classList.remove("xyz");
+
+//element = document.querySelector("#header").classList;
+
+//console.log(element);
---
+++ DOM Challenge Starting Files/targeting.js
@@ -0,0 +1,37 @@
+//TARGETING / FINDING / SELECTING AN HTML ELEMENT (JS DOM)
+
+// element = document.all;
+
+element = document.head;
+
+//element = document.title;
+
+//element = document.body;
+
+//element = document.links;
+
+//element = document.links[0];
+
+//element = document.images;
+
+//  element = document.forms;
+
+//element = document.doctype;
+
+//element = document.URL;
+
+//element = document.domain;
+
+//element = document.baseURI;
+
+//element = document.getElementById("header");
+
+//element = document.getElementsByClassName("list");
+
+//element = document.getElementsByClassName("list")[0]
+
+//element = document.getElementsByTagName("ul");
+
+// element = document.getElementsByTagName("ul")[2]
+
+console.log(element);
